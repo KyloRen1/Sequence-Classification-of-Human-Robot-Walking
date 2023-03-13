af7d8397ca41458a62c7b2450f5f1bff98672257
KyloRen1 <ivanyuk.bogdan1999@gmail.com>
2023-03-13 13:58:18 +0100

    test pipeline fix

--- test.py
+++ test.py
@@ -57,9 +57,11 @@ if __name__ == "__main__":
     )
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
     model = create_model(experiment_config).to(device)
-    model.load_state_dict(torch.load(args.checkpoint_path))
+    pretrained_weights = torch.load(args.checkpoint_path, map_location=device)
+    model.load_state_dict(pretrained_weights)
+    print('loaded weights')
+    
 
     test_model(
         experiment_config,
